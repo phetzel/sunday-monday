23eadb95bd0d8547e2f0a4078c734775b9b4a4b5
Phil <phetzel89@gmail.com>
2021-01-05 09:40:27 -0800

    About Content

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -10,6 +10,19 @@ const About = () => {
             </div>
 
             <div className="header-rect" />
+
+            <div className="about">
+                <p>
+                    Founded on the philosophy that there is a inherent connection
+                    between music and visual art, Spacemother is a label designed to
+                    be a home for all kinds of creative beings. Our artists represent
+                    the diversity in our creative world, drawing from a multiverse of
+                    backgrounds and talents, and spawning all manner of expression.
+                    Brought together in the embrace of the Spacemother, they created
+                    new and collaborative works, with the singular goal of celebrating
+                    the whole of creation.
+                </p>
+            </div>
         </div>
     )
 }
