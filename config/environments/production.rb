7a2988d9869d130d35d3b91d0f73a9355ccf7c57
Phil <phetzel89@gmail.com>
2021-02-04 18:27:39 -0800

    Sendgrid Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     :user_name      => 'apikey',
-    :password       => Rails.application.credentials.sendgrid.api_key,
+    :password       => Rails.application.credentials.sendgrid[:api_key],
     :domain         => 'thespacemother.com',
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
